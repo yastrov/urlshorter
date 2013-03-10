7decbbfe62bc4c8a00fd2cd95dd64eb3aa88023c
Yuri Astrov <yuriastrov@gmail.com>
2013-03-10 19:13:16 +0400

    Basic functionality

--- shorter/models.py
+++ shorter/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Url(models.Model):
-    url = models.URLField()
+    url = models.URLField(unique = True)
     short_version = models.AutoField(primary_key=True)
     pub_date = models.DateTimeField('date published')
 
