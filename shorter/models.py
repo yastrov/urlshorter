dd6de182b5531f821f2656774174326b6e2f7ed7
Yuri Astrov <yuriastrov@gmail.com>
2013-03-10 19:19:20 +0400

    Revert "Basic functionality"
    This reverts commit 7decbbfe62bc4c8a00fd2cd95dd64eb3aa88023c.

--- shorter/models.py
+++ shorter/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Url(models.Model):
-    url = models.URLField(unique = True)
+    url = models.URLField()
     short_version = models.AutoField(primary_key=True)
     pub_date = models.DateTimeField('date published')
 
